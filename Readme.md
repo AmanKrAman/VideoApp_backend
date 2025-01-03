23f7a0cafc6683cc16a1e2d69310b5c8f15757e2
aman kumar <aman.k@ninestars.in>
2025-01-03 08:53:19 +0530

    v1.3.9 added user_route and user_controller

--- Readme.md
+++ Readme.md
@@ -1,4 +1,4 @@
-#v1.3.7
+#v1.3.9
 #backend project sample 1
 
 - [Model link](https://app.eraser.io/workspace/YtPqZ1VogxGy1jzIDkzj)
--- src/app.js
+++ src/app.js
@@ -13,4 +13,14 @@ app.use(express.urlencoded({extended: true , limit : "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+
+//routes
+import userRouter from "./routes/user.routes.js"
+
+
+//routes declaration
+//can't use get wrong practice  we have written middleware so take it via midleware so use use
+app.use("/api/v1/users" , userRouter)
+
+
 export {app}
