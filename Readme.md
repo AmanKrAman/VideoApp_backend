365dae404e58e3707947d1ca31ce9d83d0e9817d
aman kumar <aman.k@ninestars.in>
2025-01-03 12:31:50 +0530

    v1.3.11 bug fix all register_user

--- Readme.md
+++ Readme.md
@@ -1,4 +1,4 @@
-#v1.3.10
+#v1.3.11
 #backend project sample 1
 
 - [Model link](https://app.eraser.io/workspace/YtPqZ1VogxGy1jzIDkzj)
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -12,7 +12,7 @@ const registerUser = asyncHandler(async (req, res) => {
     ){
         throw new ApiError(400, "All fields are required")
     }
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{username},{email}]
     })
     if(existedUser){
@@ -20,7 +20,11 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar file is required.")
@@ -29,15 +33,15 @@ const registerUser = asyncHandler(async (req, res) => {
     const avatar = await uploadOnCloudinary(avatarLocalPath)
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
-    if (avatar) {
+    if (!avatar) {
         throw new ApiError(400, "Avatar file is required.")
     }
 
     const user = await User.create({
-        fullname, 
+        fullname,
         avatar: avatar.url,
-        coverImage: coverImage?.url ||"",
-        email,
+        coverImage: coverImage?.url || "",
+        email, 
         password,
         username: username.toLowerCase()
     })
